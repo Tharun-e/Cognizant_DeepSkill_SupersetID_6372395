4ad9d93ec6c14c4eb76e00c35ea6c677394fdc66
Tharun-e <tharunesw26@gmail.com>
2025-06-29 15:53:18 +0530

    --

---
+++ Week2/MockTestProject/Coding/src/main/java/Ex_1_Mocking_and_Stubbing/ExternalApi.java
@@ -0,0 +1,8 @@
+package Ex_1_Mocking_and_Stubbing;
+
+public class ExternalApi {
+    public String getData() {
+        // Dummy return or leave empty if only for mocking
+        return "Real Data";
+    }
+}
