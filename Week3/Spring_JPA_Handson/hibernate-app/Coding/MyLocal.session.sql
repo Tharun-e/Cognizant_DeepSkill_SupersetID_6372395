a40430bd97c6bcbb8ce81da47f3cadd0c6d8a2aa
Tharun-e <tharunesw26@gmail.com>
2025-07-27 11:29:50 +0530

    week6

--- Week3/Spring_JPA_Handson/hibernate-app/Coding/MyLocal.session.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE employees (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    department VARCHAR(100)
-);
-INSERT INTO employee (name, department) VALUES ('Alice', 'Finance');
-INSERT INTO employee (name, department) VALUES ('Bob', 'Marketing');
-SELECT*from employees;
